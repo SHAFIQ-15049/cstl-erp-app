45be2d64d2fb8b214a1567ab5800481164319de8
monju <monjurmorshed793@gmail.com>
2020-12-12 23:50:37 +0600

    Continued work on training

--- src/main/webapp/app/app-components/training-ext/training-ext.component.ts
+++ src/main/webapp/app/app-components/training-ext/training-ext.component.ts
@@ -18,6 +18,8 @@ import {TrainingComponent} from "app/entities/training/training.component";
 })
 export class TrainingExtComponent extends TrainingComponent implements OnInit, OnDestroy {
 
+  employeeId?: number | null;
+
   constructor(
     protected trainingService: TrainingExtService,
     protected activatedRoute: ActivatedRoute,
@@ -26,6 +28,47 @@ export class TrainingExtComponent extends TrainingComponent implements OnInit, O
     protected modalService: NgbModal
   ) {
     super(trainingService, activatedRoute, router, eventManager, modalService);
+  }
+
+  loadPage(page?: number, dontNavigate?: boolean): void {
+    const pageToLoad: number = page || this.page || 1;
+    if(this.employeeId){
+      this.trainingService
+        .query({
+          page: pageToLoad - 1,
+          size: this.itemsPerPage,
+          sort: ['id,asc'],
+          'employeeId.equals': this.employeeId,
+        })
+        .subscribe(
+          (res: HttpResponse<ITraining[]>) => this.onSuccess(res.body, res.headers, pageToLoad, !dontNavigate),
+          () => this.onError()
+        );
+    }
+
+  }
+
+  protected handleNavigation(): void {
+    combineLatest(this.activatedRoute.data, this.activatedRoute.queryParamMap, (data: Data, params: ParamMap) => {
+      this.employeeId = +params.get('employeeId')!;
+      const page = params.get('page');
+      const pageNumber = page !== null ? +page : 1;
+      const sort = (params.get('sort') ?? data['defaultSort']).split(',');
+      const predicate = sort[0];
+      const ascending = sort[1] === 'asc';
+      if (pageNumber !== this.page || predicate !== this.predicate || ascending !== this.ascending) {
+        this.predicate = predicate;
+        this.ascending = ascending;
+        this.loadPage(pageNumber, true);
+      }
+    }).subscribe();
+  }
+
+  protected onSuccess(data: ITraining[] | null, headers: HttpHeaders, page: number, navigate: boolean): void {
+    this.totalItems = Number(headers.get('X-Total-Count'));
+    this.page = page;
+    this.trainings = data || [];
+    this.ngbPaginationPage = this.page;
   }
 
 }
