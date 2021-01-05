bb2a91b212af4991481fdb565aaf0bd0d82de53f
monju <monjurmorshed793@gmail.com>
2021-01-05 14:42:21 +0600

    payroll management service added

--- src/main/java/software/cstl/web/rest/PayrollManagementResource.java
+++ src/main/java/software/cstl/web/rest/PayrollManagementResource.java
@@ -3,8 +3,10 @@ package software.cstl.web.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import software.cstl.domain.enumeration.MonthType;
 import software.cstl.service.mediators.PayrollService;
 
 /**
@@ -25,8 +27,9 @@ public class PayrollManagementResource {
     /**
     * GET generateEmptySalaries
     */
-    @GetMapping("/generate-empty-salaries")
-    public String generateEmptySalaries() {
+    @GetMapping("/generate-empty-salaries/year/{year}/month/{month}/designation/{designationId}")
+    public String generateEmptySalaries(@PathVariable Integer year, @PathVariable MonthType month, @PathVariable Long designationId) {
+        payrollService.createEmptyMonthlySalaries(year, month, designationId);
         return "generateEmptySalaries";
     }
 
