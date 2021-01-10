3b3a6546d47a2856f25404f302cb67b82e7013b2
monju <monjurmorshed793@gmail.com>
2021-01-10 16:47:43 +0600

    Random changes

---
+++ src/test/java/software/cstl/service/mediators/PayrollServiceTest.java
@@ -0,0 +1,11 @@
+package software.cstl.service.mediators;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import software.cstl.CodeNodeErpApp;
+
+
+public class PayrollServiceTest {
+
+}
