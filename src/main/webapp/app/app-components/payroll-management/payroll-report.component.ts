a2ac9442b1d2c1cac116fea4a2b7c38cf4b0ad9e
Monjur-E-Morshed <monjurmorshed793@gmail.com>
2021-02-18 01:19:12 +0600

    Continued work on payroll report

--- src/main/webapp/app/app-components/payroll-management/payroll-report.component.ts
+++ src/main/webapp/app/app-components/payroll-management/payroll-report.component.ts
@@ -6,6 +6,7 @@ import { MonthlySalaryDtlService } from 'app/entities/monthly-salary-dtl/monthly
 import { DepartmentService } from 'app/entities/department/department.service';
 import { JhiAlertService } from 'ng-jhipster';
 import { DesignationService } from 'app/entities/designation/designation.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'jhi-payroll-report',
@@ -28,5 +29,21 @@ export class PayrollReportComponent implements OnInit {
     private designationService: DesignationService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.configureYears();
+    forkJoin(this.departmentService.query({ size: 1000 }), this.designationService.query({ size: 1000 })).subscribe(res => {
+      this.departments = res[0].body ? res[0].body : [];
+      this.designations = res[1].body ? res[1].body : [];
+    });
+  }
+
+  configureYears(): void {
+    let year = new Date().getFullYear();
+    this.selectedYear = new Date().getFullYear();
+    this.years.push(year);
+    for (let i = 0; i < 3; i++) {
+      year -= 1;
+      this.years.push(year);
+    }
+  }
 }
